ede316fdd418ca66b7880cc1d1e459cd297efbe6
Ryan Wallace <107794887+ryanwallace64@users.noreply.github.com>
2023-06-09 19:45:12 -0500

    Add Higher Lower game

---
+++ higher_lower/main.py
@@ -0,0 +1,50 @@
+import random
+import game_data
+import art
+from replit import clear
+
+def new_person():
+    return random.choice(game_data.data) 
+
+def game():
+    print(art.logo)
+    score = 0
+    compare_b = new_person()
+
+    end_game = False
+    while not end_game:
+        compare_a = compare_b
+        compare_b = new_person()
+        while compare_b == compare_a:
+            compare_b = new_person()
+    
+        if compare_a["follower_count"] > compare_b["follower_count"]:
+            correct_answer = "A"
+        else:
+            correct_answer = "B"
+       
+        print(f"Compare A: {compare_a['name']}, {compare_a['description']}, {compare_a['country']}.")
+        print(art.vs)
+        print(f"Against B: {compare_b['name']}, {compare_b['description']}, {compare_b['country']}.")
+        guess = input("Who has more followers? 'A' or 'B': ")
+
+        if guess == correct_answer:
+            score += 1
+            clear()
+            print(art.logo)
+            print(f"You're right! Currect score: {score}.")
+        else:
+            clear()
+            print(art.logo)
+            print(f"You're wrong. You lose! Your score was: {score}.")
+            end_game = True
+        
+keep_playing = True
+while keep_playing == True:
+    game()
+    if input("Would you like to play again? 'y' or 'n': ") == "n":
+        keep_playing = False
+    else:
+        clear()
+
+print("Thanks for playing!")

3f30eb2962ddcea6ad42705dbcc9a244c09e74f0
Ryan Wallace <107794887+ryanwallace64@users.noreply.github.com>
2023-06-16 14:48:12 -0500

    Add food, scoreboard, and gameover functionality

--- games/snake/main.py
+++ games/snake/main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,9 +11,11 @@ screen.title("My Snake Game")
 screen.tracer(0)
 segments = []
 screen.listen()
+scoreboard = Scoreboard()
 
 
 snake = Snake()
+food = Food()
 
 game_is_on = True
 while game_is_on:
@@ -19,6 +23,19 @@ while game_is_on:
     time.sleep(.1)
 
     snake.move()
+    if snake.head.distance(food) < 17:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < - 290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
     screen.onkey(snake.up, "Up")
     screen.onkey(snake.down, "Down")

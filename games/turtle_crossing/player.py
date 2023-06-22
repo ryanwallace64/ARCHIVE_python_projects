8f223155d3b280733d4142ecb2c815f83e83ff0f
Ryan Wallace <107794887+ryanwallace64@users.noreply.github.com>
2023-06-22 12:49:03 -0500

    Update player.py

--- games/turtle_crossing/player.py
+++ games/turtle_crossing/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.new_turtle()
+
+    def new_turtle(self):
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
